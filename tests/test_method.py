8a4b43b031889a006d358e979bc2cbe1b476f249
Takuya Sawada <takuya@tuntunkun.com>
2018-05-06 00:48:41 +0900

    move tests code out of package

--- tests/test_method.py
+++ tests/test_method.py
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # 
-from ..r53update import R53UpdateApp
+from r53update import R53UpdateApp
 import dns.resolver
 import netifaces
 import unittest
